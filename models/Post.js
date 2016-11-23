c03545085623f09482a7659e7dec507086178d82
waffoss <thewaffoss@gmail.com>
2016-11-23 11:02:17 +0200

    Adding Post model.

---
+++ models/Post.js
@@ -0,0 +1,36 @@
+const mongoose = require('mongoose');
+
+let postSchema = mongoose.Schema(
+    {
+        title:{type: String, required: true},
+        content:{type: String},
+        author:{type: mongoose.Schema.Types.ObjectId, required: true,ref:'User'},
+        imgUrl:{type: String},
+        date:{type: Date, default: Date.now()}
+    }
+);
+
+postSchema.method({
+    prepareInsert: function () {
+        let User = mongoose.model('User');
+        User.findById(this.author).then(user =>{
+            if(user){
+                user.articles.push(this.id);
+                user.save();
+            }
+        });
+    },
+
+    prepareDelete: function () {
+        let User = mongoose.model('User');
+        User.findById(this.author).then(user =>{
+            if(user){
+                user.article.remove(this.id);
+                user.save();
+            }
+        });
+    }
+});
+
+const Post = mongoose.model('Post', postSchema);
+module.exports = Post;
